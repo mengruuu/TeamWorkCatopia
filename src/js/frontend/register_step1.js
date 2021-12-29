f411e25612f9f185163a34cc3fb620415cfcd903
Nicholasneal12345 <410203041@gms.ndhu.edu.tw>
2021-12-29 19:31:18 +0800

    <修改>將register_step1除了按鈕以外改成使用vue來寫

--- src/js/frontend/register_step1.js
+++ src/js/frontend/register_step1.js
@@ -1,7 +1,41 @@
 const register_step1_next_step_button = document.querySelector("button.register_step1_next_step_button");
-const show_and_hide_icon = document.querySelector("#show_and_hide_icon");
-const hide = show_and_hide_icon.children[1];
-const show =show_and_hide_icon.children[2];
+// const show_and_hide_icon = document.querySelector("#show_and_hide_icon");
+// const hide = show_and_hide_icon.children[1];
+// const show =show_and_hide_icon.children[2];
+const vm = new Vue({
+    el: "#register_step1_app",
+    template: `
+        <div>
+            <div id = "show_and_hide_icon" @click = "changeHideAndShow">
+                <label>重新輸入密碼</label>
+                <i :class="{far: true, 'fa-eye-slash': true, hide_close: isShow}"></i>
+                <i :class="{far: true, 'fa-eye': true, register_first_icon_hide: true, show_open: isShow}"></i>
+            </div>
+            <input :type = "inputType">
+        </div>
+    `,
+    data() {
+        return {
+            isShow: false,
+            inputType: "password"
+        }
+    },
+    methods: {
+        changeHideAndShow() {
+            if(this.isShow === false) {
+                this.isShow = true;
+                if(this.inputType === "password") {
+                    this.inputType = "text";
+                }
+            }else {
+                this.isShow = false;
+                if(this.inputType === "text") {
+                    this.inputType = "password";
+                }
+            }
+        },
+    },
+});
 
 
 
@@ -10,13 +44,13 @@ register_step1_next_step_button.addEventListener("click", function () {
     window.location.href = "/dist/register_step2.html";
 });
 
-show_and_hide_icon.addEventListener("click", function() {
-    hide.classList.toggle("hide_close");
-    show.classList.toggle("show_open");
+// show_and_hide_icon.addEventListener("click", function() {
+//     hide.classList.toggle("hide_close");
+//     show.classList.toggle("show_open");
 
-    if(hide.classList.contains("hide_close")) {
-        show_and_hide_icon.nextElementSibling.type = "text";
-    }else {
-        show_and_hide_icon.nextElementSibling.type = "password";
-    }
-});
+//     if(hide.classList.contains("hide_close")) {
+//         show_and_hide_icon.nextElementSibling.type = "text";
+//     }else {
+//         show_and_hide_icon.nextElementSibling.type = "password";
+//     }
+// });
