d39e8d1fbcc6d7477d94c07e29ba1d2e237ad11f
bibirock <bibircok0104@gmail.com>
2022-01-06 11:04:56 +0800

    memecat<修改>商品內頁微調

--- src/js/frontend/inside.js
+++ src/js/frontend/inside.js
@@ -1,14 +1,14 @@
-const vm = new Vue({
-    el:"inside_first_block_commodity"
-});
+let big_img = document.getElementsByClassName("big_img");
+      let small_img = document.getElementsByClassName("small_img");
 
-//點擊圖片切換
-// let big_img = document.getElementsByClassName("big_img");
-// let small_img = document.getElementsByClassName("small_img");
+      small_img[0].style = 'transform: scale(1.1);border:1px solid;border-radius: 5px;';
+      for( let i=0; i < small_img.length; i++){
+      small_img[i].addEventListener("click", function(){
+        big_img[0].src = small_img[i].src;
+        for(let i=0; i < small_img.length; i++){
+          small_img[i].style='';
+        }
+        this.style = 'transform: scale(1.1);border:1px solid;border-radius: 5px;';
+      });
+      };
 
-// for( let i=0; i < small_img.length; i++){
-//     small_img[i].addEventListener("click", function(e){
-//         big_img[0].src = small_img[i].src;
-       
-//     });
-// }
