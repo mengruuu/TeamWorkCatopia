d6c91f63eb2a849e855005052e3e28bef68e2ded
bibirock <bibircok0104@gmail.com>
2022-01-06 13:53:11 +0800

    memecat<修改> 商品內頁

--- src/js/frontend/inside.js
+++ src/js/frontend/inside.js
@@ -1,14 +1,17 @@
-let big_img = document.getElementsByClassName("big_img");
-      let small_img = document.getElementsByClassName("small_img");
+function doQuery() {
+  let big_img = document.getElementsByClassName("big_img")[0];
+  let small_img = document.getElementsByClassName("small_img");
+  small_img[0].style =
+    "transform: scale(1.1);border:2px solid #774F40;border-radius: 5px;";
 
-      small_img[0].style = 'transform: scale(1.1);border:1px solid;border-radius: 5px;';
-      for( let i=0; i < small_img.length; i++){
-      small_img[i].addEventListener("click", function(){
-        big_img[0].src = small_img[i].src;
-        for(let i=0; i < small_img.length; i++){
-          small_img[i].style='';
-        }
-        this.style = 'transform: scale(1.1);border:1px solid;border-radius: 5px;';
-      });
-      };
-
+  for (let i = 0; i < small_img.length; i++) {
+    small_img[i].addEventListener("click", function () {
+      big_img.src = small_img[i].src;
+      for (let i = 0; i < small_img.length; i++) {
+        small_img[i].style = "";
+      }
+      this.style = "transform: scale(1.1);border:2px solid #774F40;border-radius: 5px;";
+    });
+  }
+}
+document.addEventListener("load", doQuery());
