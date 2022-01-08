c972b8adaf68664790141694c7993f41bae7dd23
bibirock <bibircok0104@gmail.com>
2022-01-08 17:01:50 +0800

    memecat<修改>問題機器人可以問答了 <新增>兩個資料庫,txt

--- src/frontend/API/question.php
+++ src/frontend/API/question.php
@@ -2,15 +2,16 @@
 
 //開資料庫
 include("../library/Connection.php");;
-
-
+   $question = htmlspecialchars($_GET["question"]);
    //建立SQL語法
-   $sql = "SELECT * FROM ROBOT;";
+   $sql = "SELECT * FROM ROBOT WHERE QUESTION_CONTENT like ?";
        
     //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
     //$statement = $pdo->query($sql);
-    $statement = $pdo->query($sql);
-    //getConn("dev")誇號裡面可以放入選擇的環境，讓switch去判斷現在該連在哪個資料庫
+    $statement = $pdo->prepare($sql);
+    //模糊收尋
+    $statement->bindValue(1,"%".$question."%");
+
     $statement->execute();
        //抓出全部且依照順序封裝成一個二維陣列
        $data = $statement->fetchAll();
