10aa8c4408ef5af7c0b505eee41e944ca414c1b8
mengruuu <1nozomi2haha3@gmail.com>
2022-01-10 16:00:21 +0800

    Ru<修改>後台上架功能

--- src/frontend/API/allProducts.php
+++ src/frontend/API/allProducts.php
@@ -1,11 +1,7 @@
 <?php
     $db_host = "127.0.0.1";
     $db_user = "root";
-<<<<<<< HEAD
-    $db_pass = "gtr123456";
-=======
     $db_pass = "gl4su06T";
->>>>>>> 5eceeff596c91b60120e482f9d9ffaf07b2254b3
     $db_select = "CATOPIA";
     $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
     $pdo = new PDO($dsn, $db_user, $db_pass);
