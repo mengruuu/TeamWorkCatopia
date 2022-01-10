c6c0f18b2010f92c0c38374c91ddce3197895193
Nicholasneal12345 <410203041@gms.ndhu.edu.tw>
2022-01-10 11:34:54 +0800

    <修改>新增發文功能，修改留言版的css樣式

--- src/js/frontend/message.js
+++ src/js/frontend/message.js
@@ -5,130 +5,33 @@ const click_to_message_input_container = document.querySelector("input.click_to_
 const message_write_message_container = document.querySelector("div.message_write_message_container");
 const message_write_message_close = document.querySelector("div.message_write_message_close");
 const message_write_background = document.querySelector("div.message_write_background");
-const inputFile = document.querySelector("input[type = 'file");
+const inputFile = document.querySelector("input[type = 'file']");
+const message_write_message_content = document.querySelector("textarea.message_write_message_content");
+const confirmPost = document.querySelector("#confirmPost");
 const message_write_img_container_content = document.querySelector("div.message_write_img_container_content");
 
-function upLoadImg(srcEl, distEl) {
-    srcEl.addEventListener("change", function () {
-        const file = document.querySelector("input[type = 'file']").files[0];console.log(file.type);
-        const readFile = new FileReader();
 
 
-
-
-        distEl.classList.add("message_write_img_container_overflowY");
-        readFile.readAsDataURL(file);
-        readFile.addEventListener("load", function () {
-            if (distEl.children[0].children[1]) {
-                distEl.children[0].children[1].remove();
-            }
-            distEl.children[0].insertAdjacentHTML("beforeend", `
-                            <img class = "message_write_img_content" src = ${readFile.result}>
-                        `);
-        });
-    });
-};
-
-function dragOverImg(message_write_message_content_container, el, newClass) {
-    el.addEventListener("dragover", function (event) {
-        event.preventDefault();
-        el.classList.add(newClass);
-    });
-
-    // 拖曳檔案到內容區的時候，圖片上傳區就會顯示出來
-    message_write_message_content_container.addEventListener("dragover", function (event) {
-        event.preventDefault();
-        message_write_img_container.classList.add("message_write_img_container_open");
-    });
-
-}
-
-function droppedImg(srcEl, message_write_img_container) {
-    // 拖曳檔案到圖片上傳區的時候，圖片就能上傳
-    srcEl.addEventListener("drop", function (event) {
-        event.preventDefault();
-        const file = event.dataTransfer.files[0];
-        const readFile = new FileReader();
-
-
-
-
-        message_write_img_container.classList.add("message_write_img_container_overflowY");
-        readFile.readAsDataURL(file);
-        readFile.addEventListener("load", function () {
-            if (srcEl.children[1]) {
-                srcEl.children[1].remove();
-            }
-            srcEl.insertAdjacentHTML("beforeend", `
-                            <img class = "message_write_img_content" src = ${readFile.result}>
-                        `);
-        });
-    });
-}
-
-
-
-
-//檢查會員是否登入
-login_check();
-let response = 1; //先假設會員為1
-
-// 上傳圖片
-upLoadImg(inputFile, message_write_img_container);
-dragOverImg(message_write_img_container.parentElement, message_write_img_container.children[0], "message_write_img_container_open");
-droppedImg(message_write_img_container.children[0], message_write_img_container);
-
-// 點擊上傳圖片按鈕後，顯示圖片上傳區
-message_write_message_img_file_container.addEventListener("click", function () {
-    message_write_img_container.classList.add("message_write_img_container_open");
-});
-
-// 點擊發一則貼文後的效果
-click_to_message_input_container.addEventListener("click", function () {
-    message_write_message_container.classList.add("message_write_message_container_open");
-    message_write_background.style.display = "block";
-});
-
-// 點擊貼文關閉按鈕後的效果
-message_write_message_close.addEventListener("click", function () {
-    message_write_message_container.classList.remove("message_write_message_container_open");
-    message_write_background.style.display = "none";
-});
-
-// 點擊外面其他元素來關閉發文區
-document.querySelector("body").addEventListener("click", function (event) {
-    if (message_write_message_container.classList.contains("message_write_message_container_open") && event.target === message_write_background) {
-        message_write_message_container.classList.remove("message_write_message_container_open");
-        message_write_background.style.display = "none";
-    }
-}, true);
-
-// 點擊圖片右上方的X會做的事情
-message_write_img_container.children[0].addEventListener("click", function () {
-    if (!(message_write_img_container.children[0].children[1])) {
-        window.alert("目前沒有選擇圖片！");
-    } else {
-        if (window.confirm("確定要刪除圖片？")) {
-            message_write_img_container.classList.remove("message_write_img_container_open");
-            message_write_img_container.children[0].children[1].remove();
-        } else {
-            return;
-        }
-    }
-});
 
 async function uploadMessageData() {
     const messageInfo = 
         await fetch("./API/messageGetInfo.php")
         .then(res =>res.json())
         .then(data=> data); //獲取留言版的資訊
-    console.log(messageInfo);
+    
+    //檢查會員是否登入
+    login_check();
+    const response = 
+              await fetch("./API/get_member_info.php")
+                    .then(res => res.json())
+                    .then(data => data); //取得會員資料
+    console.log(response);
 
+    // 顯示留言板的部分，使用Vue寫--------------------------------------------------------------------------------------
     const Feature = Vue.component('messageContent', {
         data() {
             return{
                 isListShow: false,
-                isPostHide: false
             }
         },
         template:`
@@ -191,11 +94,14 @@ async function uploadMessageData() {
             },
             index: {
                 type: Number
+            },
+            isPostHide: {
+                type: Boolean
             }
         },
         computed: {
-            settingIsHide() {console.log(typeof(this.memberId), typeof(response));
-                if(Number(this.memberId) === response) {console.log("y");
+            settingIsHide() {
+                if(this.memberId === response[0].MEMBER_ID) {console.log("y");
                     return false;
                 }else {console.log("n");
                     return true;
@@ -213,7 +119,6 @@ async function uploadMessageData() {
             deletePost() {
                 if(confirm("確定要刪除貼文？")) {
                     this.$emit('deletepost', this.postId);
-                    this.isPostHide = true;
                 }else {
                     return;
                 }
@@ -233,17 +138,20 @@ async function uploadMessageData() {
                     :postContent = "message.POST_CONTENT"
                     :postId = "message.POST_ID"
                     :index = "index"
+                    :isPostHide = "isPostHide"
                     @deletepost = "deletePost"
                 />
             </div>
         `,
         data() {
             return {
-                messageInfo: messageInfo
+                messageInfo: messageInfo,
+                isPostHide: false
             }
         },
         methods: {
             deletePost(postID) {
+                // this.isPostHide = true;
                 setTimeout(function() {
                     fetch("./API/deleteMessage.php", {
                         method: "POST",
@@ -260,6 +168,147 @@ async function uploadMessageData() {
             }
         }
     });
+
+    // po文章跟上傳圖片等部分-------------------------------------------------------------------------------------------
+    let postInfo = {
+        postContent: "",
+        postImg: "",
+        memberId: response[0].MEMBER_ID
+    };
+
+    function upLoadImg(srcEl, distEl) {
+        srcEl.addEventListener("change", function () {
+            const file = document.querySelector("input[type = 'file']").files[0];
+            const readFile = new FileReader();
+    
+    
+    
+    
+            distEl.classList.add("message_write_img_container_overflowY");
+            readFile.readAsDataURL(file);
+            readFile.addEventListener("load", function () {
+                if (distEl.children[0].children[1]) {
+                    distEl.children[0].children[1].remove();
+                }
+
+                distEl.children[0].insertAdjacentHTML("beforeend", `
+                    <img class = "message_write_img_content" src = ${readFile.result}>
+                `);
+
+                postInfo.postImg = readFile.result;
+            });
+        });
+    };
+    
+    function dragOverImg(message_write_message_content_container, el, newClass) {
+        el.addEventListener("dragover", function (event) {
+            event.preventDefault();
+            el.classList.add(newClass);
+        });
+    
+        // 拖曳檔案到內容區的時候，圖片上傳區就會顯示出來
+        message_write_message_content_container.addEventListener("dragover", function (event) {
+            event.preventDefault();
+            message_write_img_container.classList.add("message_write_img_container_open");
+        });
+    
+    }
+    
+    function droppedImg(srcEl, message_write_img_container) {
+        // 拖曳檔案到圖片上傳區的時候，圖片就能上傳
+        srcEl.addEventListener("drop", function (event) {
+            event.preventDefault();
+            const file = event.dataTransfer.files[0];
+            const readFile = new FileReader();
+    
+    
+    
+    
+            message_write_img_container.classList.add("message_write_img_container_overflowY");
+            readFile.readAsDataURL(file);
+            readFile.addEventListener("load", function () {
+                if (srcEl.children[1]) {
+                    srcEl.children[1].remove();
+                }
+
+                srcEl.insertAdjacentHTML("beforeend", `
+                    <img class = "message_write_img_content" src = ${readFile.result}>
+                `);
+
+                postInfo.postImg = readFile.result;
+            });
+        });
+    }
+    
+    
+    
+    
+    
+    // 上傳圖片
+    upLoadImg(inputFile, message_write_img_container);
+    dragOverImg(message_write_img_container.parentElement, message_write_img_container.children[0], "message_write_img_container_open");
+    droppedImg(message_write_img_container.children[0], message_write_img_container);
+
+    // 點擊送出po文------------------------------------------------------------------------------------------------------
+    confirmPost.addEventListener("click", function() {
+        postInfo.postContent = message_write_message_content.value;
+        if(postInfo.postContent === "" || postInfo.postImg === "") {
+            alert("未發文或上傳圖片！");
+        }else {
+            fetch("./API/createPost.php", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(postInfo)
+            })
+            .then(res => res.json())
+            .then(data => {console.log(data);
+                window.alert("上傳成功");
+                location.href = "/TeamWorkCatopia/dist/message.html";
+            })
+        }
+    });
+
+    // 更改class以及刪除圖片等部分--------------------------------------------------------------------------------------------------------
+    // 點擊上傳圖片按鈕後，顯示圖片上傳區
+    message_write_message_img_file_container.addEventListener("click", function () {
+        message_write_img_container.classList.add("message_write_img_container_open");
+    });
+    
+    // 點擊發一則貼文後的效果
+    click_to_message_input_container.addEventListener("click", function () {
+        message_write_message_container.classList.add("message_write_message_container_open");
+        message_write_background.style.display = "block";
+    });
+    
+    // 點擊貼文關閉按鈕後的效果
+    message_write_message_close.addEventListener("click", function () {
+        message_write_message_container.classList.remove("message_write_message_container_open");
+        message_write_background.style.display = "none";
+    });
+    
+    // 點擊外面其他元素來關閉發文區
+    document.querySelector("body").addEventListener("click", function (event) {
+        if (message_write_message_container.classList.contains("message_write_message_container_open") && event.target === message_write_background) {
+            message_write_message_container.classList.remove("message_write_message_container_open");
+            message_write_background.style.display = "none";
+        }
+    }, true);
+    
+    // 點擊圖片右上方的X會做的事情
+    message_write_img_container.children[0].addEventListener("click", function () {
+        if (!(message_write_img_container.children[0].children[1])) {
+            window.alert("目前沒有選擇圖片！");
+        } else {
+            if (window.confirm("確定要刪除圖片？")) {
+                message_write_img_container.classList.remove("message_write_img_container_open");
+                message_write_img_container.children[0].children[1].remove();
+            } else {
+                return;
+            }
+        }
+    });    
 }
 
 uploadMessageData();
