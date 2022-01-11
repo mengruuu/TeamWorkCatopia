e3a2e9654f04441ec2f0ce80be70d2e42effd012
bibirock <bibircok0104@gmail.com>
2022-01-11 16:48:46 +0800

    memecat<新增>後台問答機器人{新、刪、修改}

---
+++ src/backend/API/b7_question_edit_delete.php
@@ -0,0 +1,17 @@
+<?php
+
+include("../../library/Connection.php");
+
+$question_id = $_GET["question_id"];
+// 建立SQL語法
+$sql = "DELETE FROM CATOPIA.ROBOT  WHERE QUESTION_ID = ?";
+
+echo($question_id);
+// 執行
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1 , $question_id);
+$statement->execute();
+
+echo ('修改成功')
+
+?>
