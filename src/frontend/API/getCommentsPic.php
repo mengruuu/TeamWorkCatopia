48a741d015b225adf50cb024c68bd65fcbbdc2c7
Nicholasneal12345 <410203041@gms.ndhu.edu.tw>
2022-01-13 01:05:47 +0800

    <修改>新增留言圖片功能 <新增>新增留言圖片獲取相關的php檔案

---
+++ src/frontend/API/getCommentsPic.php
@@ -0,0 +1,14 @@
+<?php
+    include("../library/member.php");
+    include("../library/Connection.php");
+    $MEMBER_ID = getMemberID();
+    $get_member_info = "SELECT MEMBER_PICTURE FROM MEMBER WHERE MEMBER_ID = ?";
+    $statement_info = $pdo -> prepare($get_member_info);
+    $statement_info ->bindValue(1, $MEMBER_ID);
+    $statement_info -> execute();  
+    $data_member_info = $statement_info -> fetchAll();
+    if($data_member_info >0){
+        echo json_encode($data_member_info);
+    }
+
+?>
--- src/js/frontend/message.js
+++ src/js/frontend/message.js
@@ -14,21 +14,21 @@ const message_write_img_container_content = document.querySelector("div.message_
 
 
 async function uploadMessageData() {
-    const messageInfo = 
+    const messageInfo =
         await fetch("./API/messageGetInfo.php")
-        .then(res =>res.json())
-        .then(data=> data); //獲取留言版的資訊
-    
+        .then(res => res.json())
+        .then(data => data); //獲取留言版的資訊
+
     //檢查會員是否登入
     login_check();
-    const response = 
-              await fetch("./API/get_member_info.php")
-                    .then(res => res.json())
-                    .then(data => data); //取得會員資料
-                    console.log("memberID: ", response[0].MEMBER_ID);
+    const response =
+        await fetch("./API/get_member_info.php")
+        .then(res => res.json())
+        .then(data => data); //取得會員資料
+    console.log("memberID: ", response[0].MEMBER_ID);
 
-    const personalLikes = 
-        await fetch("./API/getMessageLikesAndComments.php",{
+    const personalLikes =
+        await fetch("./API/getMessageLikesAndComments.php", {
             method: "POST",
             headers: {
                 "content-type": "application/json"
@@ -37,21 +37,43 @@ async function uploadMessageData() {
         })
         .then(res => res.json())
         .then(data => data);
-        console.log("likeHistory: ", personalLikes);
 
-    const personalComments = 
+    const personalComments =
         await fetch("./API/getComments.php")
+        .then(res => res.json())
+        .then(data => data);
+
+    const personalCommentsId =
+        await fetch("./API/getCommentsId.php")
+        .then(res => res.json())
+        .then(data => data);
+
+    let personalCommentsPic = [];
+
+    for (let i = 0; i < personalCommentsId.length; i = i + 1) {
+        let pic =
+            await fetch("./API/getCommentsPic.php", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(personalCommentsId[i]['RESPONSE&LIKE_MEMBER_ID'])
+            })
             .then(res => res.json())
             .then(data => data);
+        personalCommentsPic.push(pic[0].MEMBER_PICTURE);
+    }
+
+    console.log("personalCommentsPic: ", personalCommentsPic);
 
     // 顯示留言板的部分，使用Vue寫--------------------------------------------------------------------------------------
     const Feature = Vue.component('messageContent', {
         data() {
-            return{
+            return {
                 isListShow: false,
             }
         },
-        template:`
+        template: `
             <div :data_postId = "postId" :class = "{message_container: true, message_container_hide: isPostHide}" style = "background-image: url(./images/message/message_background_img.png);">
                 <div :class = "{message_setting_container: true, message_setting_container_hide: settingIsHide}">
                     <div :class = "{message_setting_container_content: true}" @click = "listOpenOrClose">
@@ -84,11 +106,12 @@ async function uploadMessageData() {
                     <img :class = "{message_content_img: true}" :src = postPicture>
                     <input :class = "{comment_input: true}" placeholder = "回應貼文..." @keyup = "inputComment">
                     <div :class = "{message_comment_container: true}" v-for = "(comment, index) in comments">
+                        <img :class = "{message_comment_user_img: true}" :src = "commentsPic[index]">
                         <p :class = "{message_comment_item: true}">{{ comment.comment }}</p>
                     </div>
                 </div>
             </div>
-        `, 
+        `,
         // <input :class = "{comment_input" placeholder: true}" placeholder = "回應貼文...">
         // <div :class = "{message_comment_container: true}">
         // <img :class = "{message_comment_user_img: true}" src = "./images/message/message_comment_photo.svg">
@@ -128,41 +151,44 @@ async function uploadMessageData() {
             },
             comments: {
                 type: Array
+            },
+            commentsPic: {
+                type: Array
             }
         },
         computed: {
             settingIsHide() {
-                if(this.memberId === response[0].MEMBER_ID) {
+                if (this.memberId === response[0].MEMBER_ID) {
                     return false;
-                }else {console.log("n");
+                } else {
                     return true;
                 }
             }
         },
         methods: {
             listOpenOrClose() {
-                if(this.isListShow) {
+                if (this.isListShow) {
                     this.isListShow = false;
-                }else {
+                } else {
                     this.isListShow = true;
                 }
             },
             deletePost() {
-                if(confirm("確定要刪除貼文？")) {
+                if (confirm("確定要刪除貼文？")) {
                     this.$emit('deletepost', this.postId);
-                }else {
+                } else {
                     return;
                 }
             },
             changeLikeImg() {
-                if(this.personalLikes) {
+                if (this.personalLikes) {
                     this.$emit("changeLikeCounts", this.postId, Number(this.postLike) - 1, this.index);
-                }else {
+                } else {
                     this.$emit("changeLikeCounts", this.postId, Number(this.postLike) + 1, this.index);
                 }
             },
             inputComment(e) {
-                if(e.keyCode === 13 && e.target.value){
+                if (e.keyCode === 13 && e.target.value) {
                     const inputValue = e.target.value;
 
                     e.target.value = "";
@@ -171,7 +197,7 @@ async function uploadMessageData() {
             }
         },
     });
-    
+
     const vm = new Vue({
         el: "#message_app",
         template: `
@@ -189,7 +215,7 @@ async function uploadMessageData() {
                     :isPostHide = "isPostHide"
                     :personalLikes = "isLiked(index)"
                     :comments = "updateComments(index)"
-                    :commentsPic = "upDateCommentsPic"
+                    :commentsPic = "personalCommentsPic"
                     @deletepost = "deletePost"
                     @changeLikeCounts = "changelikecounts"
                     @insertComment = "insertcomment"
@@ -201,6 +227,7 @@ async function uploadMessageData() {
                 messageInfo: messageInfo,
                 personalLikes: personalLikes,
                 personalComments: personalComments,
+                personalCommentsPic: personalCommentsPic,
                 isPostHide: false
             }
         },
@@ -212,32 +239,45 @@ async function uploadMessageData() {
                     postId: postId
                 }
                 await fetch("./API/insertComment.php", {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(res => res.json())
-                .then(data => { 
-                    vm.personalComments = data;
-                });
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        vm.personalComments = data;
+                    });
+
+                await fetch("./API/getCommentsPic.php", {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(memberId)
+                    })
+                    .then(res => res.json())
+                    .then(data => { console.log("personalCommentsPic: ", data);
+                        vm.personalCommentsPic.push(data[0].MEMBER_PICTURE);
+                    });
 
                 this.updateComments(index);
             },
             deletePost(postID) {
                 // setTimeout(function() {
                 fetch("./API/deleteMessage.php", {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(postID)
-                })
-                .then(res => res.json())
-                .then(data => {console.log(data);
-                    vm.messageInfo = data;
-                });
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(postID)
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        vm.messageInfo = data;
+                    });
                 // }, 1000);
             },
             async changelikecounts(postID, updateLikeCounts, index) {
@@ -248,38 +288,39 @@ async function uploadMessageData() {
                 }
 
                 await fetch("./API/updateLikeCounts.php", {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(postIdAndUpdateLikeCounts)
-                })
-                .then(res => res.json())
-                .then(data => {console.log(data);
-                    vm.messageInfo = data;
-                });
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(postIdAndUpdateLikeCounts)
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        vm.messageInfo = data;
+                    });
 
-                await fetch("./API/getMessageLikesAndComments.php",{
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(response[0].MEMBER_ID)
-                })
-                .then(res => res.json())
-                .then(data => {
-                    console.log("data: ", data);
-                    console.log("personalLikesBefore: ", vm.personalLikes);
-                    vm.personalLikes = data;
-                    console.log("data: ", data);
-                    console.log("personalLikesAfter: ", vm.personalLikes);
-                });
+                await fetch("./API/getMessageLikesAndComments.php", {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify(response[0].MEMBER_ID)
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log("data: ", data);
+                        console.log("personalLikesBefore: ", vm.personalLikes);
+                        vm.personalLikes = data;
+                        console.log("data: ", data);
+                        console.log("personalLikesAfter: ", vm.personalLikes);
+                    });
             },
             isLiked(index) {
                 let isLiked = false;
 
-                for(let i = 0; i < this.personalLikes.length; i = i + 1) {
-                    if(this.personalLikes[i].POST_ID === this.messageInfo[index].POST_ID && this.personalLikes[i].LIKE_MODE === "1") {
+                for (let i = 0; i < this.personalLikes.length; i = i + 1) {
+                    if (this.personalLikes[i].POST_ID === this.messageInfo[index].POST_ID && this.personalLikes[i].LIKE_MODE === "1") {
                         isLiked = true;
                         break;
                     }
@@ -290,9 +331,9 @@ async function uploadMessageData() {
             updateComments(index) {
                 let comments = [];
 
-                for(let i = 0; i < this.personalComments.length; i = i + 1) {
-                    if(this.personalComments[i].POST_ID === this.messageInfo[index].POST_ID) {
-                        if(this.personalComments[i].POST_RESPONSE_CONTENT){
+                for (let i = 0; i < this.personalComments.length; i = i + 1) {
+                    if (this.personalComments[i].POST_ID === this.messageInfo[index].POST_ID) {
+                        if (this.personalComments[i].POST_RESPONSE_CONTENT) {
                             const data = {
                                 id: this.personalComments[i]["RESPONSE&LIKE_MEMBER_ID"],
                                 comment: this.personalComments[i].POST_RESPONSE_CONTENT
@@ -302,11 +343,8 @@ async function uploadMessageData() {
                     }
                 }
 
-                this.upDateCommentsPic(comments);
-                
                 return comments;
-            },
-            upDateCommentsPic(comments) {}
+            }
         }
     });
 
@@ -321,10 +359,10 @@ async function uploadMessageData() {
         srcEl.addEventListener("change", function () {
             const file = document.querySelector("input[type = 'file']").files[0];
             const readFile = new FileReader();
-    
-    
-    
-    
+
+
+
+
             distEl.classList.add("message_write_img_container_overflowY");
             readFile.readAsDataURL(file);
             readFile.addEventListener("load", function () {
@@ -336,35 +374,36 @@ async function uploadMessageData() {
                     <img class = "message_write_img_content" src = ${readFile.result}>
                 `);
 
-                postInfo.postImg = readFile.result; console.log(readFile.result);
+                postInfo.postImg = readFile.result;
+                console.log(readFile.result);
             });
         });
     };
-    
+
     function dragOverImg(message_write_message_content_container, el, newClass) {
         el.addEventListener("dragover", function (event) {
             event.preventDefault();
             el.classList.add(newClass);
         });
-    
+
         // 拖曳檔案到內容區的時候，圖片上傳區就會顯示出來
         message_write_message_content_container.addEventListener("dragover", function (event) {
             event.preventDefault();
             message_write_img_container.classList.add("message_write_img_container_open");
         });
-    
+
     }
-    
+
     function droppedImg(srcEl, message_write_img_container) {
         // 拖曳檔案到圖片上傳區的時候，圖片就能上傳
         srcEl.addEventListener("drop", function (event) {
             event.preventDefault();
             const file = event.dataTransfer.files[0];
             const readFile = new FileReader();
-    
-    
-    
-    
+
+
+
+
             message_write_img_container.classList.add("message_write_img_container_overflowY");
             readFile.readAsDataURL(file);
             readFile.addEventListener("load", function () {
@@ -380,34 +419,35 @@ async function uploadMessageData() {
             });
         });
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     // 上傳圖片
     upLoadImg(inputFile, message_write_img_container);
     dragOverImg(message_write_img_container.parentElement, message_write_img_container.children[0], "message_write_img_container_open");
     droppedImg(message_write_img_container.children[0], message_write_img_container);
 
     // 點擊送出po文------------------------------------------------------------------------------------------------------
-    confirmPost.addEventListener("click", function() {
+    confirmPost.addEventListener("click", function () {
         postInfo.postContent = message_write_message_content.value;
-        if(postInfo.postContent === "" || postInfo.postImg === "") {
+        if (postInfo.postContent === "" || postInfo.postImg === "") {
             alert("未發文或上傳圖片！");
-        }else {
+        } else {
             fetch("./API/createPost.php", {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify(postInfo)
-            })
-            .then(res => res.json())
-            .then(data => {console.log(data);
-                window.alert("上傳成功");
-                location.href = "/TeamWorkCatopia/dist/message.html";
-            })
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(postInfo)
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    window.alert("上傳成功");
+                    location.href = "/TeamWorkCatopia/dist/message.html";
+                })
         }
     });
 
@@ -416,19 +456,19 @@ async function uploadMessageData() {
     message_write_message_img_file_container.addEventListener("click", function () {
         message_write_img_container.classList.add("message_write_img_container_open");
     });
-    
+
     // 點擊發一則貼文後的效果
     click_to_message_input_container.addEventListener("click", function () {
         message_write_message_container.classList.add("message_write_message_container_open");
         message_write_background.style.display = "block";
     });
-    
+
     // 點擊貼文關閉按鈕後的效果
     message_write_message_close.addEventListener("click", function () {
         message_write_message_container.classList.remove("message_write_message_container_open");
         message_write_background.style.display = "none";
     });
-    
+
     // 點擊外面其他元素來關閉發文區
     document.querySelector("body").addEventListener("click", function (event) {
         if (message_write_message_container.classList.contains("message_write_message_container_open") && event.target === message_write_background) {
@@ -436,7 +476,7 @@ async function uploadMessageData() {
             message_write_background.style.display = "none";
         }
     }, true);
-    
+
     // 點擊圖片右上方的X會做的事情
     message_write_img_container.children[0].addEventListener("click", function () {
         if (!(message_write_img_container.children[0].children[1])) {
@@ -449,7 +489,7 @@ async function uploadMessageData() {
                 return;
             }
         }
-    });    
+    });
 }
 
 uploadMessageData();
