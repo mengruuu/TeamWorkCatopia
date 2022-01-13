6e98dddbf29396a95562a4ee386307d110882692
Wuhsinyun <selina.wu.1130@gmail.com>
2022-01-13 21:34:29 +0800

    心云 <新增>後台 撈會員訂單資料+訂單詳細

---
+++ src/backend/API/b2_back_order_detail.php
@@ -0,0 +1,21 @@
+<?php
+    include("../../library/Connection.php");
+
+    //抓取資料
+    $ORDER_ID = $_POST['order_id'];
+    $check;
+
+    $get_order_info = "SELECT * FROM CATOPIA.`V_PRODUCT_ORDER_DETAIL`
+    WHERE `ORDER_ID` = ?";
+
+    $statement_order_info = $pdo -> prepare($get_order_info);
+
+    $statement_order_info->bindValue(1, $ORDER_ID);
+
+    $statement_order_info -> execute();
+    
+    $data_order_info = $statement_order_info -> fetchAll();
+    if($data_order_info >0){
+        echo json_encode($data_order_info);
+    }
+?>
